946d707819b3ed864e13cc88b0269c99027fdd33
jeevanpillay <169354619+jeevanpillay@users.noreply.github.com>
2025-03-25 10:51:49 +1100

    init

---
+++ src/config/site.ts
@@ -0,0 +1,15 @@
+import { SiteConfig } from "~/types/site";
+
+export const siteConfig: SiteConfig<"github"> = {
+  name: "Jeevan Pillay",
+  url: "https://jeevanpillay.com",
+  ogImage: "https://jeevanpillay.com/og.jpg",
+  description:
+    "Graudated in software engineering, Jeevan Pillay is a passionate artist turned developer who is passionate about product design and building delightful user experiences.",
+  links: {
+    github: {
+      title: "GitHub",
+      href: "https://github.com/jeevanpillay/polychromos",
+    },
+  },
+};
